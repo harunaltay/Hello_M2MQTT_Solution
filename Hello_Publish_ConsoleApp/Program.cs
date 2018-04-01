0f83f484cca2795a23f92bb26afe20b8975575be
Harun Altay <h.altay@gmail.com>
2018-04-01 22:09:14 +0300

    mesaj

--- Hello_Publish_ConsoleApp/Program.cs
+++ Hello_Publish_ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace Hello_Publish_ConsoleApp
 {
@@ -10,6 +13,20 @@ namespace Hello_Publish_ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba MQTT");
+
+            // create client instance
+            MqttClient client = new MqttClient(IPAddress.Parse("127.0.0.1"));
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            //string strValue = Convert.ToString(value);
+            string strValue = "Merhaba_VS_M2MQTT";
+
+            // publish a message on "/home/temperature" topic with QoS 2
+            client.Publish("/hello", Encoding.UTF8.GetBytes(strValue)  /*, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE */ );
+
         }
     }
 }
---
+++ Hello_Subscribe_ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Hello_Subscribe_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Subscriber - MQTT");
+
+            MqttClient client = new MqttClient(IPAddress.Parse("127.0.0.1"));
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            // subscribe to the topic "/home/temperature" with QoS 2
+            client.Subscribe
+                (new string[] { "/hello" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+
+        }
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // access data bytes throug e.Message
+            byte[] buffer = e.Message;
+            string s = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+            Console.WriteLine(s);
+        }
+    }
+}
