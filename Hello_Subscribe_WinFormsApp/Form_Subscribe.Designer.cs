c05babec53c9754fdc337f1f050d54d89a7f9a6f
Harun Altay <h.altay@gmail.com>
2018-04-02 00:24:04 +0300

    winforms subscribe denn sonra

--- Hello_Subscribe_WinFormsApp/Form_Subscribe.Designer.cs
+++ Hello_Subscribe_WinFormsApp/Form_Subscribe.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Hello_Publish_WinFormsApp
+﻿namespace Hello_Subscribe_WinFormsApp
 {
-    partial class Form_Publish
+    partial class Form_Subscribe
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Location = new System.Drawing.Point(270, 192);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(219, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Click Me to Publish!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.Location = new System.Drawing.Point(50, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(696, 353);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // Form_Publish
+            // Form_Subscribe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form_Publish";
-            this.Text = "Form - Publish M2MQTT";
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form_Subscribe";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Subscribe_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
---
+++ Hello_Subscribe_WinFormsApp/Form_Subscribe.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Hello_Subscribe_WinFormsApp
+{
+    public partial class Form_Subscribe : Form
+    {
+        MqttClient client;
+
+        public Form_Subscribe()
+        {
+            InitializeComponent();
+
+            client = new MqttClient(IPAddress.Parse("127.0.0.1"));
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            // subscribe to the topic "/home/temperature" with QoS 2
+            client.Subscribe
+                (new string[] { "/hello" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        private void Form_Subscribe_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // access data bytes throug e.Message
+            byte[] buffer = e.Message;
+            string s = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+            //Console.WriteLine(s);
+
+            textBox1.Text += s + "\n";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
